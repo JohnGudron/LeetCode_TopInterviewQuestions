d86d66afb8e21e7cf3e57e94dce0ca234b2e1b9a
JohnGudron <oak43556@gmail.com>
2022-12-22 01:14:53 +0300

    Add code with "Trees" issues solutions (class Trees). Committing just for practice in GIT

--- src/Trees.kt
+++ src/Trees.kt
@@ -7,6 +7,10 @@ class Trees {
         var right: TreeNode? = null
     }
 
+    class Node(var value: Int) {
+        var next: Node? = null
+    }
+
     // https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
     fun maxDepth(root: TreeNode?): Int {
         if (root == null) return 0
@@ -50,17 +54,66 @@ class Trees {
     }
 
     // https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/627/
-    /*fun isSymmetric(root: TreeNode?): Boolean {
+    fun isSymmetric(root: TreeNode?): Boolean {
+        val h = maxDepth(root)
+        if (h == 1) return true
+        if (h == 2) return root?.right?.`val` == root?.left?.`val`
+        val leftArr = mutableListOf<Int> ()
+        val rightArr = mutableListOf<Int> ()
+        fun add (node: TreeNode?, height: Int, direction: String) {
+            if (direction == "left") {
+                leftArr.add(node?.`val` ?: -101)
+            } else {
+                rightArr.add(node?.`val` ?: -101)
+            }
+            if (height == h ) return
+            if (direction == "left") {
+                add(node?.left, height + 1, direction)
+                add(node?.right, height + 1, direction)
+            } else {
+                add(node?.right, height + 1, direction)
+                add(node?.left, height + 1, direction)
+            }
 
-    }*/
+        }
+
+        add(root?.left, 2, "left")
+        add(root?.right, 2, "right")
+
+        return leftArr == rightArr
+    }
 
     // https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/628/
-    /*fun levelOrder(root: TreeNode?): List<List<Int>> {
-
-    }*/
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        if (root == null) return emptyList()
+        val result = mutableListOf<MutableList<Int>>()
+        val level = 0
+        fun add (node: TreeNode?, level: Int) {
+            if (node == null) return
+            if (result.getOrNull(level) == null ) result.add(mutableListOf())
+            result[level].add(node.`val`)
+            add(node.left, level + 1)
+            add(node.right, level + 1)
+        }
+        add(root, level)
+        return result
+    }
 
     // https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/631/
-    /*fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        if (nums.size == 1) return TreeNode(nums[0])
+        if (nums.isEmpty()) return null
+        val root = TreeNode(nums[nums.size/2])
+        root.left = sortedArrayToBST(nums.sliceArray(0 until (nums.size/2)))
+        root.right = sortedArrayToBST(nums.sliceArray((nums.size/2 + 1)..nums.lastIndex))
 
-    }*/
+        /*fun distrib (node:TreeNode) {
+            root.left = sortedArrayToBST(nums.sliceArray(0 until (nums.size/2)))
+            root.right = sortedArrayToBST(nums.sliceArray((nums.size/2 + 1)..nums.lastIndex))
+        }*/
+
+        return root
+    }
+    // -9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9
+
 }
